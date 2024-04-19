076f35a69a6734a76e8c0ffcefda653b8a132917
MBadriNarayanan <MBadriNarayanan>
2024-04-19 16:57:50 -0500

    [ UPDATE ] - Utils to compute metrics

--- Scripts/utils.py
+++ Scripts/utils.py
@@ -1,7 +1,8 @@
 import ast
 import os
 
-import pandas as pd
+from collections import Counter
+from difflib import SequenceMatcher
 
 
 def create_directory(directory, print_flag=True):
@@ -62,17 +63,70 @@ def extract_triplets(text):
     return triplets
 
 
-def get_entity_relation_dataframe(df, column_name):
-    dataframe = pd.DataFrame(columns=["e1", "e2", "relation"])
+def get_entity_relation_data(data):
+    entity_relation_data = []
+    data = ast.literal_eval(data)
+    for triplet in data:
+        e1 = triplet.get("head", "").strip()
+        relation = triplet.get("type", "").strip()
+        e2 = triplet.get("tail", "").strip()
+        entity_relation_data.append({"e1": e1, "e2": e2, "relation": relation})
+    return entity_relation_data
 
-    for _, row in df.iterrows():
-        data = ast.literal_eval(row[column_name])
-        for triplet in data:
-            e1 = triplet.get("head", "").strip()
-            relation = triplet.get("type", "").strip()
-            e2 = triplet.get("tail", "").strip()
-            triplet_data = pd.DataFrame(
-                {"e1": [e1], "e2": [e2], "relation": [relation]}
-            )
-            dataframe = pd.concat([dataframe, triplet_data], ignore_index=True)
-    return dataframe
+
+def get_precision_recall_f1_score(ground_truth, prediction):
+    ground_truth = Counter(ground_truth.lower().strip())
+    prediction = Counter(prediction.lower().strip())
+
+    tp = sum((ground_truth & prediction).values())
+    fp = sum((prediction - ground_truth).values())
+    fn = sum((ground_truth - prediction).values())
+
+    precision = 0.0
+    recall = 0.0
+    f1_score = 0.0
+
+    if tp + fp != 0:
+        precision = tp / float(tp + fp)
+
+    if tp + fn != 0:
+        recall = tp / float(tp + fn)
+
+    if precision != 0 and recall != 0:
+        f1_score = (2 * precision * recall) / float(precision + recall)
+
+    precision = round(precision, 3)
+    recall = round(recall, 3)
+    f1_score = round(f1_score, 3)
+    return precision, recall, f1_score
+
+
+def rank_extractions(ground_truth, prediction):
+    ground_truth = ground_truth[0]
+    e1_gt = ground_truth["e1"]
+    e2_gt = ground_truth["e2"]
+    relation_gt = ground_truth["relation"]
+    ground_truth = "{} {} {}".format(e1_gt, relation_gt, e2_gt)
+    score_list = []
+
+    for idx, data in enumerate(prediction):
+        e1_pred = data["e1"]
+        e2_pred = data["e2"]
+        relation_pred = data["relation"]
+
+        e1_similarity = SequenceMatcher(None, e1_pred.lower(), e1_gt.lower()).ratio()
+        e2_similarity = SequenceMatcher(None, e2_pred.lower(), e2_gt.lower()).ratio()
+        relation_similarity = SequenceMatcher(
+            None, relation_pred.lower(), relation_gt.lower()
+        ).ratio()
+
+        similarity_score = (
+            (0.33 * e1_similarity)
+            + (0.33 * e2_similarity)
+            + (0.34 * relation_similarity)
+        )
+        similarity_score = round(similarity_score, 3)
+        score_list.append([similarity_score, idx])
+    score_list.sort()
+    score_list = score_list[::-1]
+    return score_list, ground_truth

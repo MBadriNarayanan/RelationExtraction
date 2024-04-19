6cb256c03d34cb40000e531f7a599e3284bed8c6
MBadriNarayanan <MBadriNarayanan>
2024-04-19 16:58:06 -0500

    [ UPDATE ] - Metrics Script

--- Scripts/metrics.py
+++ Scripts/metrics.py
@@ -3,74 +3,79 @@ import os
 
 import pandas as pd
 
-from difflib import SequenceMatcher
 from tqdm import tqdm
-from utils import get_entity_relation_dataframe
+from utils import (
+    get_entity_relation_data,
+    get_precision_recall_f1_score,
+    rank_extractions,
+)
 
 
-def compute_metrics(
-    input_filename, output_filepath, e1_threshold, e2_threshold, relation_threshold
-):
+def clean_data(input_filename, output_csv_path):
+    df = pd.read_csv(input_filename)
+
+    ground_truth_list = []
+    prediction_list = []
+
+    for _, row in tqdm(df.iterrows()):
+        ground_truth = get_entity_relation_data(data=row["GroundTruth"])
+        prediction = get_entity_relation_data(data=row["Predictions"])
+        score_list, ground_truth = rank_extractions(
+            ground_truth=ground_truth, prediction=prediction
+        )
+        best_idx = score_list[0][1]
+        prediction = prediction[best_idx]
+        prediction = "{} {} {}".format(
+            prediction["e1"], prediction["relation"], prediction["e2"]
+        )
+
+        ground_truth_list.append(ground_truth)
+        prediction_list.append(prediction)
+
+    df["UpdatedGroundTruth"] = ground_truth_list
+    df["BestPrediction"] = prediction_list
+
+    df.to_csv(output_csv_path, index=False)
+
+
+def compute_metrics(input_filename, output_filepath):
+    write_string = "Metrics for the file: {}\n--------------------\n".format(
+        input_filename
+    )
     with open(output_filepath, "w") as report_file:
-        report_file.write("Metrics for the file: {}\n".format(input_filename))
+        report_file.write(write_string)
 
     df = pd.read_csv(input_filename)
-    tp_triplets = []
 
-    ground_truth_df = get_entity_relation_dataframe(df=df, column_name="GroundTruth")
-    predictions_df = get_entity_relation_dataframe(df=df, column_name="Predictions")
+    precision_list = []
+    recall_list = []
+    f1_score_list = []
 
-    for _, row_pred in tqdm(predictions_df.iterrows()):
-        e1_pred = row_pred["e1"]
-        e1_pred_similarities = [
-            SequenceMatcher(None, e1_pred, row_gt["e1"]).ratio()
-            for _, row_gt in ground_truth_df.iterrows()
-        ]
-        e1_match_indices = [
-            j for j, sim in enumerate(e1_pred_similarities) if sim >= e1_threshold
-        ]
+    for _, row in tqdm(df.iterrows()):
+        ground_truth = row["UpdatedGroundTruth"]
+        prediction = row["BestPrediction"]
+        precision, recall, f1_score = get_precision_recall_f1_score(
+            ground_truth=ground_truth, prediction=prediction
+        )
 
-        for j in e1_match_indices:
-            e2_pred = row_pred["e2"]
-            e2_gt = ground_truth_df.iloc[j]["e2"]
+        precision_list.append(precision)
+        recall_list.append(recall)
+        f1_score_list.append(f1_score)
 
-            e2_similarity = SequenceMatcher(None, e2_pred, e2_gt).ratio()
+    precision = sum(precision_list) / len(precision_list)
+    recall = sum(recall_list) / len(recall_list)
+    f1_score = sum(f1_score_list) / len(f1_score_list)
 
-            if e2_similarity >= e2_threshold:
-                relation_pred = row_pred["relation"]
-                relation_gold = ground_truth_df.iloc[j]["relation"]
-                relation_similarity = SequenceMatcher(
-                    None, relation_pred, relation_gold
-                ).ratio()
-                if relation_similarity >= relation_threshold:
-                    result_triplet = {
-                        "e1": e1_pred,
-                        "e2": e2_pred,
-                        "relation": "{} | {} matches".format(
-                            relation_gold, relation_pred
-                        ),
-                    }
-                    tp_triplets.append(result_triplet)
-                else:
-                    result_triplet = {
-                        "e1": e1_pred,
-                        "e2": e2_pred,
-                        "relation": "{} | {} does not match".format(
-                            relation_gold, relation_pred
-                        ),
-                    }
-                with open(output_filepath, "at") as report_file:
-                    report_file.write("Result Triplet: {}\n".format(result_triplet))
-                    report_file.write("--------------------\n")
+    df["Precision"] = precision_list
+    df["Recall"] = recall_list
+    df["F1-Score"] = f1_score_list
 
-    precision = len(tp_triplets) / float(predictions_df.shape[0])
-    recall = len(tp_triplets) / float(ground_truth_df.shape[0])
-    f1_score = (2 * precision * recall) / float(precision + recall)
+    df.to_csv(input_filename, index=False)
 
     with open(output_filepath, "at") as report_file:
-        report_file.write("Precision: {:.3f}\n".format(precision))
-        report_file.write("Recall: {:.3f}\n".format(recall))
-        report_file.write("F1-Score: {:.3f}\n".format(f1_score))
+        report_file.write("Avg Precision: {:.3f}\n".format(precision))
+        report_file.write("Avg Recall: {:.3f}\n".format(recall))
+        report_file.write("Avg F1-Score: {:.3f}\n".format(f1_score))
         report_file.write("--------------------\n")
 
 
@@ -92,46 +97,29 @@ if __name__ == "__main__":
         help="Output Directory to store metrics report",
         required=True,
     )
-    parser.add_argument(
-        "--e1",
-        "-E",
-        type=float,
-        help="E1 Threshold for metrics computation",
-        default=0.7,
-    )
-    parser.add_argument(
-        "--e2",
-        "-T",
-        type=float,
-        help="E2 Threshold for metrics computation",
-        default=0.5,
-    )
-    parser.add_argument(
-        "--relation",
-        "-R",
-        type=float,
-        help="Relation Threshold for metrics computation",
-        default=0.4,
-    )
     args = parser.parse_args()
 
     input_filename = args.input
     output_directory = args.output
-    e1_threshold = args.e1
-    e2_threshold = args.e2
-    relation_threshold = args.relation
+
+    output_csv_filename = "{}_baseline_metrics.csv".format(
+        input_filename.split("/")[-1].split(".")[0]
+    )
+    output_csv_path = os.path.join(output_directory, output_csv_filename)
 
     output_filename = "{}_baseline_report.txt".format(
         input_filename.split("/")[-1].split(".")[0]
     )
     output_filepath = os.path.join(output_directory, output_filename)
 
-    compute_metrics(
+    clean_data(
         input_filename=input_filename,
+        output_csv_path=output_csv_path,
+    )
+
+    compute_metrics(
+        input_filename=output_csv_path,
         output_filepath=output_filepath,
-        e1_threshold=e1_threshold,
-        e2_threshold=e2_threshold,
-        relation_threshold=relation_threshold,
     )
     print(
         "\n--------------------\nComputed metrics successfully!\n--------------------\n"

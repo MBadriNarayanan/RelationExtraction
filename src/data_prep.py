50d3aa631aaf0273923d903c6ad770d99a15d2b6
MBadriNarayanan <MBadriNarayanan>
2024-11-20 18:01:03 -0600

    [ UPDATE ] - Format Data Prep Script

--- src/data_prep.py
+++ src/data_prep.py
@@ -1,5 +1,5 @@
-import numpy as np
 import json
+import os
 from tqdm import tqdm
 
 example = "[{'subject': 'sub', 'predicate': 'pred', 'object': 'obj'}]"
@@ -10,7 +10,9 @@ def data_prompt(user_msg, model_answer):
         "messages": [
             {
                 "role": "system",
-                "content": f"Given the following text, print out a list of triplets in the following JSON format: {example}",
+                "content": "Given the following text, print out a list of triplets in the following JSON format: {}".format(
+                    example
+                ),
             },
             {"role": "user", "content": user_msg},
             {"role": "assistant", "content": json.dumps(model_answer)},
@@ -18,21 +20,39 @@ def data_prompt(user_msg, model_answer):
     }
 
 
-for filename in ["en_train_reduced.jsonl", "en_val.jsonl", "en_test_reduced.jsonl"]:
-    prefix = filename.split(".")[0]
-    with open(filename) as f:
-        with open(f"{prefix}_llama.jsonl", "w") as out:
-            for line in tqdm(f):
-                dd = json.loads(line[0:-1])
-                text = dd["text"]
-                triples = dd["triples"]
-                trip_simple = []
-                for trip in triples:
-                    trip_simple.append(
-                        {
-                            "subject": trip["subject"]["surfaceform"],
-                            "predicate": trip["predicate"]["surfaceform"],
-                            "object": trip["object"]["surfaceform"],
-                        }
+def main():
+    print("\n--------------------\nStarting data preparation!\n--------------------\n")
+
+    valid_files = ["en_train_reduced.jsonl", "en_val.jsonl", "en_test_reduced.jsonl"]
+
+    for valid_filename in tqdm(valid_files):
+        file_prefix = valid_filename.split(".")[0]
+        file_prefix = file_prefix.split("_")[1]
+        valid_filepath = os.path.join("data", valid_filename)
+        with open(valid_filepath) as json_file:
+            json_filename = "llama_{}.jsonl".format(file_prefix)
+            json_filepath = os.path.join("prepared_data", json_filename)
+            with open(json_filepath, "w") as output_json_file:
+                for line in json_file:
+                    data = json.loads(line[0:-1])
+                    text = data["text"]
+                    triples = data["triples"]
+                    triplet = []
+                    for trip in triples:
+                        triplet.append(
+                            {
+                                "subject": trip["subject"]["surfaceform"],
+                                "predicate": trip["predicate"]["surfaceform"],
+                                "object": trip["object"]["surfaceform"],
+                            }
+                        )
+                    output_json_file.write(
+                        json.dumps(data_prompt(user_msg=text, model_answer=triples))
+                        + "\n"
                     )
-                out.write(json.dumps(data_prompt(text, triples)) + "\n")
+
+    print("\n--------------------\nData preparation completed!\n--------------------\n")
+
+
+if __name__ == "__main__":
+    main()

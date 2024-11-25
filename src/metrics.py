10cc8bf4711cd2a13ef916ec494d677b44527cd5
MBadriNarayanan <MBadriNarayanan>
2024-11-25 15:29:40 -0600

    [ UPDATE ] - Format Code using Black

--- src/metrics.py
+++ src/metrics.py
@@ -3,19 +3,21 @@ import os
 import json
 import re
 
+
 def parse_text(text):
     blob = text.split("assistant\n\n")[-1]
-    blob = re.sub(']+', ']', blob)
+    blob = re.sub("]+", "]", blob)
     try:
         return json.loads(blob)
     except:
         return []
 
+
 if __name__ == "__main__":
     tp = 0
     fp = 0
     fn = 0
-    with open('generated_samples.jsonl') as samples:
+    with open("generated_samples.jsonl") as samples:
         for l in samples:
             dd = json.loads(l[0:-1])
             preds = parse_text(dd["sample"])
@@ -28,4 +30,6 @@ if __name__ == "__main__":
             for g in gold:
                 if g not in preds:
                     fn += 1
-    print(f'precision: {tp / (tp+fp)}; recall: {tp / (tp+fn)}; f1: {2*tp / (2*tp + fp + fn)}')
+    print(
+        f"precision: {tp / (tp+fp)}; recall: {tp / (tp+fn)}; f1: {2*tp / (2*tp + fp + fn)}"
+    )

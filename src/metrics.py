8f6b07c18c9feb71c524397338f40dc2eaf52e16
Son Dam <damchaus1@gmail.com>
2024-11-26 09:50:39 -0600

    reverted metrics

--- src/metrics.py
+++ src/metrics.py
@@ -1,11 +1,12 @@
+import numpy as np
+import os
 import json
 import re
-from collections import Counter
 
 
 def parse_text(text):
     blob = text.split("assistant\n\n")[-1]
-    blob = re.sub(']+', ']', blob)
+    blob = re.sub("]+", "]", blob)
     try:
         return json.loads(blob)
     except:
@@ -16,34 +17,19 @@ if __name__ == "__main__":
     tp = 0
     fp = 0
     fn = 0
-    correct_by_relation = Counter()
-    guessed_by_relation = Counter()
-    gold_by_relation    = Counter()
-    with open('generated_samples.jsonl') as samples:
+    with open("generated_samples.jsonl") as samples:
         for l in samples:
             dd = json.loads(l[0:-1])
             preds = parse_text(dd["sample"])
             gold = json.loads(dd["gold"])
-            if gold and preds:
-                guessed_by_relation[str(preds)] += 1
-                gold_by_relation[str(gold)] += 1
-                if gold == preds:
-                    correct_by_relation[str(preds)] += 1
-            elif preds:
-                guessed_by_relation[str(preds)] += 1
-            elif gold:
-                gold_by_relation[str(gold)] += 1
-            else:
-                pass
-    prec_micro = 1.0
-    if sum(guessed_by_relation.values()) > 0:
-        prec_micro   = float(sum(correct_by_relation.values())) / float(sum(guessed_by_relation.values()))
-    recall_micro = 0.0
-    if sum(gold_by_relation.values()) > 0:
-        recall_micro = float(sum(correct_by_relation.values())) / float(sum(gold_by_relation.values()))
-    f1_micro = 0.0
-    if prec_micro + recall_micro > 0.0:
-        f1_micro = 2.0 * prec_micro * recall_micro / (prec_micro + recall_micro)
-    print("Precision (micro): {:.3%}".format(prec_micro))
-    print("   Recall (micro): {:.3%}".format(recall_micro))
-    print("       F1 (micro): {:.3%}".format(f1_micro))
+            for p in preds:
+                if p not in gold:
+                    fp += 1
+                else:
+                    tp += 1
+            for g in gold:
+                if g not in preds:
+                    fn += 1
+    print("Precision (micro): {:.3%}".format(tp / (tp+fp)))
+    print("   Recall (micro): {:.3%}".format(tp / (tp+fn)))
+    print("       F1 (micro): {:.3%}".format(2*tp / (2*tp + fp + fn)))
